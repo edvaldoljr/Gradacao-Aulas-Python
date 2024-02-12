52bbf9fde4e0141882268f1b21d02373f378512f
edvaldojr <edvaldoluizj@gmail.com>
2024-02-12 20:47:24 -0300

    listas

---
+++ lista_funcoes/lista_fiap_on/criandoLista.py
@@ -0,0 +1,9 @@
+#Lista preenchida estaticamente
+lista_estatica=["xpto", True]
+
+#Lista preenchida dinamicamente
+lista_dinamica=[input("Digite o usuário: "), bool(int(input("Está logado: ")))]
+print(lista_dinamica)
+
+#Lista vazia
+lista_vazia=[]
